30fc624f0f22780a82f59b3d527b2a6e4240f21f
Godeffroy Valet <godeffroy.valet@m4x.org>
2022-04-19 00:45:02 +0200

    plugin - added an example project.

---
+++ nl-ue4-example-project/Source/NearLinkerTest/NearLinkerTestGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "NearLinkerTestGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NEARLINKERTEST_API ANearLinkerTestGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
