2d40f14e7e9152cca704d37a6c88de6c014ab900
Liviu Marian Berciu <liviu.berciu@sectorlabs.ro>
2022-04-28 18:53:02 +0300

    Add try-catch for every endpoint

--- nl-api-server/app.js
+++ nl-api-server/app.js
@@ -13,27 +13,31 @@ const app = express();
 app.use(express.json());
 
 app.use(async (req, res, next) => {
-  const account_id = req.body.account_id;
-  const private_key = req.body.private_key;
+  try {
+    const account_id = req.body.account_id;
+    const private_key = req.body.private_key;
 
-  const keyStore = new keyStores.InMemoryKeyStore();
-  if (account_id && private_key) {
-    const keyPair = KeyPair.fromString(private_key);
+    const keyStore = new keyStores.InMemoryKeyStore();
+    if (account_id && private_key) {
+      const keyPair = KeyPair.fromString(private_key);
 
-    await keyStore
-      .setKey(config.near_config.networkId, account_id, keyPair)
-      .catch((error) => next(error));
+      await keyStore
+        .setKey(config.near_config.networkId, account_id, keyPair)
+        .catch((error) => next(error));
+    }
+    const near_config = {
+      ...config.near_config,
+      keyStore: keyStore,
+    };
+
+    const near = await connect(near_config).catch((error) => next(error));
+
+    req.near = near;
+
+    next();
+  } catch (error) {
+    next(error);
   }
-  const near_config = {
-    ...config.near_config,
-    keyStore: keyStore,
-  };
-
-  const near = await connect(near_config).catch((error) => next(error));
-
-  req.near = near;
-
-  next();
 });
 
 // Helper functions
@@ -63,63 +67,74 @@ app.get("/wallets/:wallet_id", async (req, res) => {
 app.get("/wallets/:wallet_id/balance", async (req, res) => {
   const near = req.near;
 
-  const wallet_id = req.params.wallet_id;
+  try {
+    const wallet_id = req.params.wallet_id;
 
-  await near
-    .account(wallet_id)
-    .then(async (account) => {
-      return res.status(200).send(await account.getAccountBalance());
-    })
-    .catch((error) => res.status(404).send(error));
+    await near
+      .account(wallet_id)
+      .then(async (account) => {
+        return res.status(200).send(await account.getAccountBalance());
+      })
+      .catch((error) => res.status(404).send(error));
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
 // VIEW - Call View contract function
 app.get("/contract/:contract_id/:function_name", async (req, res) => {
   const near = req.near;
+  try {
+    const contract_id = req.params.contract_id;
+    const function_name = req.params.function_name;
+    const contract_account = await near.account(contract_id);
 
-  const contract_id = req.params.contract_id;
-  const function_name = req.params.function_name;
-  const contract_account = await near.account(contract_id);
+    const result = await contract_account
+      .viewFunction(contract_id, function_name, req.query)
+      .catch((error) => {
+        console.log(error);
+        res.status(500).send("Invalid function call or parameters.");
+      });
 
-  const result = await contract_account
-    .viewFunction(contract_id, function_name, req.query)
-    .catch((error) => {
-      console.log(error);
-      res.status(500).send("Invalid function call or parameters.");
-    });
-
-  res.send(result);
+    res.send(result);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
 // CALL - Call contract function
 app.post("/contract/:contract_id/call", async (req, res) => {
   const near = req.near;
 
-  const account_id = req.body.account_id;
-  const contract_id = req.params.contract_id;
-  const function_name = req.body.function.name;
-  const params = req.body.function.parameters || {};
-  const gas = req.body.gas || "300000000000000";
-  const attached_deposit =
-    (req.body.deposit && utils.format.parseNearAmount(req.body.deposit)) ||
-    utils.format.parseNearAmount("0.1");
+  try {
+    const account_id = req.body.account_id;
+    const contract_id = req.params.contract_id;
+    const function_name = req.body.function.name;
+    const params = req.body.function.parameters || {};
+    const gas = req.body.gas || "300000000000000";
+    const attached_deposit =
+      (req.body.deposit && utils.format.parseNearAmount(req.body.deposit)) ||
+      utils.format.parseNearAmount("0.1");
 
-  const signer_account = await near.account(account_id);
+    const signer_account = await near.account(account_id);
 
-  const result = await signer_account
-    .functionCall({
-      contractId: contract_id,
-      methodName: function_name,
-      args: params,
-      gas: gas,
-      attachedDeposit: attached_deposit,
-    })
-    .catch((error) => {
-      console.error(error);
-      res.status(500).send(error);
-    });
+    const result = await signer_account
+      .functionCall({
+        contractId: contract_id,
+        methodName: function_name,
+        args: params,
+        gas: gas,
+        attachedDeposit: attached_deposit,
+      })
+      .catch((error) => {
+        console.error(error);
+        res.status(500).send(error);
+      });
 
-  res.send(result);
+    res.send(result);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
 // GET - Get transaction info
@@ -128,14 +143,18 @@ app.get(
   async (req, res) => {
     const near = req.near;
 
-    const contract_id = req.params.contract_id;
-    const transaction_id = req.params.transaction_id;
+    try {
+      const contract_id = req.params.contract_id;
+      const transaction_id = req.params.transaction_id;
 
-    const result = await near.connection.provider
-      .txStatus(transaction_id, contract_id)
-      .catch((error) => res.status(500).send(error));
+      const result = await near.connection.provider
+        .txStatus(transaction_id, contract_id)
+        .catch((error) => res.status(500).send(error));
 
-    res.send(result);
+      res.send(result);
+    } catch (error) {
+      res.status(500).send(error);
+    }
   }
 );
 
@@ -144,35 +163,42 @@ app.get(
   "/contract/:contract_id/transactions/:transaction_id",
   async (req, res) => {
     const near = req.near;
+    try {
+      const contract_id = req.params.contract_id;
+      const transaction_id = req.params.transaction_id;
 
-    const contract_id = req.params.contract_id;
-    const transaction_id = req.params.transaction_id;
+      const result = await near.connection.provider.txStatus(
+        transaction_id,
+        contract_id
+      );
 
-    const result = await near.connection.provider.txStatus(
-      transaction_id,
-      contract_id
-    );
-
-    res.send(result);
+      res.send(result);
+    } catch (error) {
+      res.status(500).send(error);
+    }
   }
 );
 
 // GET - Key pair
 app.get("/keypair", async (_, res) => {
-  const keypair = utils.KeyPair.fromRandom("ed25519");
+  try {
+    const keypair = utils.KeyPair.fromRandom("ed25519");
 
-  result = {
-    public_key: keypair.publicKey.toString(),
-    private_key: keypair.secretKey,
-  };
+    result = {
+      public_key: keypair.publicKey.toString(),
+      private_key: keypair.secretKey,
+    };
 
-  // Commented Last-Modified header since it could throw errors
-  res
-    .set("Last-Modified", new Date().toUTCString())
-    .set("Expires", new Date().toUTCString())
-    .set("Cache-Control", "no-cache")
-    .set("Pragma", "no-cache")
-    .send(result);
+    // Commented Last-Modified header since it could throw errors
+    res
+      .set("Last-Modified", new Date().toUTCString())
+      .set("Expires", new Date().toUTCString())
+      .set("Cache-Control", "no-cache")
+      .set("Pragma", "no-cache")
+      .send(result);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
 // POST - Sign URL endpoint
