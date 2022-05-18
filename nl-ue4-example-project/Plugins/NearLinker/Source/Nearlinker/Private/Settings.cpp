2f3ae14f0633fa85c6fe057db552bdae90fe8484
Godeffroy Valet <godeffroy.valet@m4x.org>
2022-05-18 13:42:03 +0200

    made a submodule from nl-ue4-example-project/Plugins/NearLinker

--- nl-ue4-example-project/Plugins/NearLinker/Source/Nearlinker/Private/Settings.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright 2022 ExxaVerse LLC. All Rights Reserved.
-
-#include"Settings.h"
-
-UNearlinkerSettings::UNearlinkerSettings(const FObjectInitializer& ObjectInitializer)
-	: Super(ObjectInitializer)
-{
-	this->contracts_directories.Add(TEXT("Content/NEAR contracts"));
-}
--- nl-ue4-example-project/Plugins/NearLinker/Source/Nearlinker/Private/log.h
+++
@@ -1,6 +0,0 @@
-// Copyright 2022 ExxaVerse LLC. All Rights Reserved.
-
-#pragma once
-
-DECLARE_LOG_CATEGORY_EXTERN(LogNearlinker, Log, All);
-
